ca938e97f9149252a5c05e469d4dcb4dd949a1a1
neequu <nist1701@gmail.com>
2023-11-04 01:41:35 +0300

    feat: setup unocss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+import '@unocss/reset/tailwind-compat.css';
+import 'uno.css';
 
 const app = createApp(App);
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
-import { fileURLToPath, URL } from 'node:url';
+// import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Component from 'unplugin-vue-components/vite';
 import AutoImport from 'unplugin-auto-import/vite';
+import UnoCSS from 'unocss/vite';
+import { presetAttributify, presetIcons, presetUno } from 'unocss';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Component(), AutoImport({ imports: ['vue'] })],
-  // resolve: {
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url)),
-  //   },
-  // },
+  plugins: [
+    vue(),
+    Component(),
+    AutoImport({ imports: ['vue'] }),
+    UnoCSS({
+      presets: [presetUno(), presetAttributify(), presetIcons()],
+    }),
+  ],
 });
+
+// resolve: {
+//   alias: {
+//     '@': fileURLToPath(new URL('./src', import.meta.url)),
+//   },
+// },
