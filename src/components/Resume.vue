705ade8c1e717648561d3abf0e751f1e314117bf
neequu <nist1701@gmail.com>
2023-11-04 23:17:08 +0300

    progress

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -10,13 +10,16 @@ const { details } = useDetailsStore()
       <h1 class="text-5xl uppercase font-bold">{{ details.name }} {{ details.lastname }}</h1>
       <p>{{ details.jobTitle }}</p>
     </div>
-    <section class="grid grid-cols-2">
-      <aside>
+    <section class="flex">
+      <aside class="border-r flex-[3]">
         <h2 class="underline text-2xl font-bold mb-2">Details</h2>
         <SubSection title="Adress" :fields="[details.city, details.country]" />
         <SubSection title="Phone" :fields="[details.phone]" />
         <SubSection title="Email" :fields="[details.email]" />
       </aside>
+      <div class="flex-[5]">
+        sdf
+      </div>
     </section>
   </div>
 </template>
